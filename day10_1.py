c50f50337fbffa55da0f2e631e3674d50184c63e
conwayjj <58484914+conwayjj@users.noreply.github.com>
2019-12-10 15:04:16 -0500

    Add files via upload

--- day10_1.py
+++ day10_1.py
@@ -44,6 +44,7 @@ for entry in maxDict:
         maxStack = len(maxDict[entry])
 
 rotDict = {}
+
 #rotate dictionary to have radians from y axis instead of radians from x axis
 for entry in maxDict:
     rotated = (2*math.pi - (entry % (2*math.pi)) ) % (2*math.pi)
@@ -51,27 +52,49 @@ for entry in maxDict:
     rotDict[rotated] = maxDict[entry]
     #print(rotated)
 
+rotDict[0].remove(maxAst)
+
 angList = []
 for key in rotDict:
     angList.append(key)
 
-outArray = [["." for x in range(xMax)] for y in range(yMax)]
+outArray = [["..." for x in range(xMax)] for y in range(yMax)]
 
 #print (angList)
 angList.sort()
 #print (angList)
-for i in range(len(angList)):
-    #print(i, angList[i], rotDict[angList[i]])
-    for ast in rotDict[angList[i]]:
-        #print(ast)
-        outArray[ast[1]][ast[0]] = "%x"%(i%16)
+zapOrder = []
+while len(angList) > 0:
+    toRemove = []
+    for i in range(len(angList)):
+        #print(i, angList[i], rotDict[angList[i]])
+        closest = 999999
+        closeAst = None
+        for ast in rotDict[angList[i]]:
+            #print(rotDict[angList[i]], ast)
+            dist = math.hypot(ast[0]-maxAst[0],ast[1]-maxAst[1])
+            if ast[0] == maxAst[0]:
+                print (ast, maxAst, dist)
+            if  dist < closest:
+                closest = dist
+                closeAst = ast
+        rotDict[angList[i]].remove(ast)
+        if len(rotDict[angList[i]]) == 0:
+            del rotDict[angList[i]]
+            toRemove.append(angList[i])
+        zapOrder.append(ast)
+    for item in toRemove:
+        angList.remove(item)
 
-outArray[maxAst[1]][maxAst[0]] = "$"
+
+for i in range(len(zapOrder)):
+    outArray[zapOrder[i][1]][zapOrder[i][0]] = '%03d' %i
+    
+outArray[maxAst[1]][maxAst[0]] = "$$$"
 
 for row in outArray:
     print (row)
 print(maxAst)
 
-print(rotDict[angList[199]])
-
-#print (outArray)
+#print (zapOrder)
+print (zapOrder[199])
